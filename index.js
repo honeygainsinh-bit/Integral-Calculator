2495b19530a21c0a895d633fb964070dcc5ee9d3
honeygainsinh-bit <honeygainsinh@gmail.com>
2025-12-11 08:26:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -211,9 +211,9 @@ OWNER_IP: process.env.OWNER_IP,
 CACHE_RATE: 0.25,
 TARGETS: {
 "Easy": 25,
-"Medium": 10,
-"Hard": 10,
-"Very Hard": 10,
+"Medium": 20,
+"Hard": 20,
+"Very Hard": 20,
 },
 TOPICS: [
 { key: "Limits", label: "លីមីត (Limits)", prompt: "Calculus Limits" },
